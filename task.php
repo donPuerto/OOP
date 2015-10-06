07c2c37c4be11e289789dc091937d76b4c1bee7f
Don Puerto <don.puerto@hotmail.com>
2015-10-06 18:11:38 +0800

    added code into task.php

---
+++ task.php
@@ -0,0 +1,17 @@
+<?php
+
+class Task {
+
+	public $description = "Task description";
+
+}
+
+// Public - this class variable is availavle publicly
+
+//Accessing class
+//create a class instances
+
+$task = new Task();
+
+echo $task->description;
+
